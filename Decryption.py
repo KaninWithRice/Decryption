6303ac65cdb9f500f7d7772f0f489b469fe2a339
KaninWithRice <mishalnorona014@gmail.com>
2023-04-20 04:18:18 +0800

    Loading Animation

--- Decryption.py
+++ Decryption.py
@@ -30,5 +30,35 @@ for x in range(len(letter_input)):
     else:
         letter_output += letter_input[x]
 # add loading time
+import time
+import sys
+
+done = 'false'
 # add animation
+def animation():
+        sys.stdout.write(Fore.RED + '\n\rDecrypting Please Wait ▒▒▒▒▒▒▒▒▒▒  0%')
+        time.sleep(0.5)
+        sys.stdout.write(Fore.RED + '\rDecrypting Please Wait █▒▒▒▒▒▒▒▒▒  10%')
+        time.sleep(0.5)
+        sys.stdout.write(Fore.RED + '\rDecrypting Please Wait ██▒▒▒▒▒▒▒▒  20%')
+        time.sleep(0.5)
+        sys.stdout.write(Fore.RED + '\rDecrypting Please Wait ███▒▒▒▒▒▒▒  30%')
+        time.sleep(0.5)
+        sys.stdout.write(Fore.RED + '\rDecrypting Please Wait ████▒▒▒▒▒▒  40%')
+        time.sleep(0.5)
+        sys.stdout.write(Fore.RED + '\rDecrypting Please Wait █████▒▒▒▒▒  50%')
+        time.sleep(0.5)
+        sys.stdout.write(Fore.RED + '\rDecrypting Please Wait ██████▒▒▒▒  60%')
+        time.sleep(0.5)
+        sys.stdout.write(Fore.RED + '\rDecrypting Please Wait ███████▒▒▒  70%')
+        time.sleep(0.5)
+        sys.stdout.write(Fore.RED + '\rDecrypting Please Wait ████████▒▒  80%')
+        time.sleep(0.5)
+        sys.stdout.write(Fore.RED + '\rDecrypting Please Wait █████████▒  90%')
+        time.sleep(0.5)
+        sys.stdout.write(Fore.RED + '\rDecrypting Please Wait ██████████ 100%')
+        time.sleep(0.3)
+        sys.stdout.write(Fore.LIGHTGREEN_EX +'\n\nDECRYPTION COMPLETE\n')
+
+animation()
 # print output
