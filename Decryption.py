e6bd1a4a27798cd0d2efeead213bd5b1e172d8a6
KaninWithRice <mishalnorona014@gmail.com>
2023-04-20 03:25:37 +0800

    Pseudocode

---
+++ Decryption.py
@@ -0,0 +1,13 @@
+# Mohammad Mishal S. Noroña | BSCPE 1-5 | PROBLEM 2 – DECRYPTION
+# add Color to the program
+# add introduction
+# ask user for input
+# check each character
+# if *, change to a
+# if &, change to e
+# if #, change to i
+# if +, change to o
+# if !, change to u
+# add loading time
+# add animation
+# print output
